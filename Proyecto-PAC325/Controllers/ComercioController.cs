06baf6d5c12d6963ea43850321df73925de0ee5b
Jose Raúl Rodríguez <jrodriguez40744@ufide.ac.cr>
2025-10-16 16:13:22 -0600

    Diseño visual del apartado de comercio (Lista)

---
+++ Proyecto-PAC325/Controllers/ComercioController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Proyecto_PAC325.Controllers
+{
+    public class ComercioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
