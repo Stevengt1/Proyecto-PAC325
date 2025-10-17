0b922f2e150c5054bc87207bfa3aaa5be56eeb4f
Jose Raúl Rodríguez <jrodriguez40744@ufide.ac.cr>
2025-10-17 14:45:42 -0600

    Eliminar archivos innecesarios

--- Proyecto-PAC325/Repository/ComercioRepository.cs
+++ Proyecto-PAC325/Repository/ComercioRepository.cs
@@ -18,5 +18,42 @@ namespace Proyecto_PAC325.Repository
             return await _context.COMERCIO.ToListAsync();
         }
 
+        public async Task<ComercioModel> GetComercio(int id)
+        {
+            return await _context.COMERCIO.FindAsync(id); //Esa funcion lo busca por el Id y devuelve el objeto 
+        }
+
+        public async Task<ComercioModel> Add(ComercioModel comercio) {
+
+            _context.COMERCIO.Add(comercio);
+            if (await _context.SaveChangesAsync() > 0) //Se valida que realmente se hayan dado cambios, esto devuelve 
+            { // la cantidad de  entidades afectadas 0, 1, 2,3 etc.. para que lo tengan en cuenta por si no entienden
+                return comercio;
+            }
+            return null;
+        }
+
+        public async Task<ComercioModel> Update(ComercioModel comercio)
+        {
+
+            _context.COMERCIO.Update(comercio);
+            if (await _context.SaveChangesAsync() > 0)
+            {
+                return comercio;
+            }
+            return null;
+        }
+
+        public async Task<ComercioModel> Delete(ComercioModel comercio)
+        {
+
+            _context.COMERCIO.Remove(comercio);
+            if (await _context.SaveChangesAsync() > 0)
+            {
+                return comercio;
+            }
+            return null;
+        }
+
     }
 }
