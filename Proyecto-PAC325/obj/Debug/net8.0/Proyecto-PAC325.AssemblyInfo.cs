6b84e17d5e1e6fcba5997584e2f0e5d89110f02b
Stevn <165967402+Stevengt1@users.noreply.github.com>
2025-11-05 18:37:25 -0600

    Sinpe Commit
    Create view

--- Proyecto-PAC325/obj/Debug/net8.0/Proyecto-PAC325.AssemblyInfo.cs
+++ Proyecto-PAC325/obj/Debug/net8.0/Proyecto-PAC325.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto-PAC325")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ad3dbb28e64577db750ebe7205d73e874d6c7e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb499de6eb68d5e630f822362afb1cc6b00fecd7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyecto-PAC325")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyecto-PAC325")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
