a2cdad547174d44a6f92f03fcb51a114832dae3d
Fanny Vargas Navarro <fannymarievargas@gmail.com>
2025-11-06 16:12:52 -0600

    Update BitacoraEvento.cs
    Agregacion de atributos de validación

--- Proyecto-PAC325/Models/BitacoraEvento.cs
+++ Proyecto-PAC325/Models/BitacoraEvento.cs
@@ -1,14 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Proyecto_PAC325.Models
 {
     public class BitacoraEvento
     {
+        [Key]
         public int IdEvento { get; set; }
+
+        [Required, MaxLength(20)]
         public string TablaDeEvento { get; set; }
+
+        [Required, MaxLength(20)]
         public string TipoDeEvento { get; set; }
+
+        [Required]
         public DateTime FechaDeEvento { get; set; }
+
+        [Required]
         public string DescripcionDeEvento { get; set; }
+
         public string? StackTrace { get; set; }
         public string? DatosAnteriores { get; set; }
         public string? DatosPosteriores { get; set; }
