65e4af9f3574b241dc2c71559c3503ab3471865b
Fanny Vargas Navarro <fannymarievargas@gmail.com>
2025-11-06 16:20:12 -0600

    BitacoraHelper.cs
    Clase se encargará de registrar automáticamente los eventos // Registrar, Editar, Eliminar o Error

--- Proyecto-PAC325/Models/BitacoraHelper.cs
+++ Proyecto-PAC325/Models/BitacoraHelper.cs
@@ -1,27 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+
 using System;
 using Proyecto_PAC325.Data;
+using Proyecto_PAC325.Models;
 using Newtonsoft.Json;
 
 namespace Proyecto_PAC325.Models
 {
     public static class BitacoraHelper
     {
-        public static void RegistrarEvento(AppDbContext context,
-                                           string tabla,
-                                           string tipo,
-                                           string descripcion,
-                                           object? datosAnteriores = null,
-                                           object? datosPosteriores = null,
-                                           Exception? ex = null)
+        public static void RegistrarEvento(
+            AppDbContext context,
+            string tabla,
+            string tipo,
+            string descripcion,
+            object? datosAnteriores = null,
+            object? datosPosteriores = null,
+            Exception? ex = null)
         {
             var evento = new BitacoraEvento
             {
                 TablaDeEvento = tabla,
                 TipoDeEvento = tipo,
                 FechaDeEvento = DateTime.Now,
-                DescripcionDeEvento = descripcion ?? string.Empty,
-                StackTrace = ex?.ToString() ?? string.Empty,
+                DescripcionDeEvento = descripcion,
+                StackTrace = ex?.StackTrace ?? string.Empty,
                 DatosAnteriores = datosAnteriores != null ? JsonConvert.SerializeObject(datosAnteriores) : null,
                 DatosPosteriores = datosPosteriores != null ? JsonConvert.SerializeObject(datosPosteriores) : null
             };
@@ -30,4 +33,5 @@ namespace Proyecto_PAC325.Models
             context.SaveChanges();
         }
     }
-}
+} 
+
